e420aeaaf49899f0a7bdb33556ce35daa94c78af
Alexandre <alexandre.azevedo@ilhasoft.com.br>
2020-12-22 07:28:56 -0300

    Added sidebar

---
+++ src/components/Sidebar/SidebarMenu.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="unnic-side-bar-menu">
+        <p class="unnic-side-bar-menu__title"> {{ text }} </p>
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'UnnicSidebarMenu',
+  props: {
+    text: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/unnic.scss';
+    .unnic-side-bar-menu {
+        color: $unnic-color-neutral-cloudy;
+        font-family: $unnic-font-family-secondary;
+        font-weight: $unnic-font-weight-regular;
+        font-size: $unnic-font-size-body-sm;
+        margin-bottom: $unnic-spacing-stack-md;
+
+        &__title {
+            margin: 0 0 $unnic-spacing-stack-xs 0;
+            font-size: $unnic-font-size-body-md;
+        }
+    }
+</style>
