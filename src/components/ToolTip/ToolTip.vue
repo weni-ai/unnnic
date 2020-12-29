e5bd09b29943a37341ee29adf5be6e3b2e6bcac6
ricardovasconcelos <ricardovasconcelos.dev@gmail.com>
2020-12-29 12:51:48 -0300

    created custom tooltip

---
+++ src/components/ToolTip/ToolTip.vue
@@ -0,0 +1,129 @@
+<template>
+<div v-bind="$attrs" v-on="$listeners" class="unnic-tooltip">
+  Hover over text
+  <span :class="[
+            'unnic-tooltip-label',
+            `unnic-tooltip-label-${side}`,
+            ]">{{text}}</span>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'unnic-tooltip',
+  props: {
+    text: {
+      type: String,
+      default: null,
+    },
+    side: {
+      type: String,
+      default: 'right',
+      validator(value) {
+        return ['top', 'right', 'bottom', 'left'].indexOf(value) !== -1;
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+ @import '../../assets/scss/unnic.scss';
+
+.unnic-tooltip {
+  position: relative;
+  display: inline-block;
+  overflow-wrap: break-word;
+}
+
+.unnic-tooltip-label{
+  z-index: 1;
+  min-width: 31px;
+  max-width: 240px;
+  visibility: hidden;
+  text-align: center;
+  position: absolute;
+
+  background-color: $unnic-color-neutral-black;
+  color: $unnic-color-neutral-snow;
+  border-radius: $unnic-border-radius-sm;
+  padding: $unnic-spacing-stack-nano $unnic-inline-nano;
+  box-shadow: $unnic-shadow-level-near;
+  font-size: $unnic-font-size-body-md;
+  font-family: $unnic-font-family-secondary;
+  font-weight: $unnic-font-weight-regular;
+  line-height: ($unnic-font-size-body-md + $unnic-line-height-medium);
+
+    &-top{
+      bottom: 150%;
+      left: 50%;
+      margin-left: -60px;
+
+      &::after {
+        content: "";
+        position: absolute;
+        top: 100%;
+        left: 50%;
+        margin-left: -5px;
+        border-width: 5px;
+        border-style: solid;
+        border-color: $unnic-color-neutral-black transparent transparent transparent;
+      }
+    }
+
+    &-bottom{
+      top: 150%;
+      left: 50%;
+      margin-left: -60px;
+
+      &::after {
+          content: "";
+          position: absolute;
+          bottom: 100%;
+          left: 50%;
+          margin-left: -5px;
+          border-width: 5px;
+          border-style: solid;
+          border-color: transparent transparent $unnic-color-neutral-black transparent;
+      }
+    }
+    &-right{
+      top: -5px;
+      left: 110%;
+
+      &::after {
+        content: "";
+        position: absolute;
+        top: 50%;
+        right: 100%;
+        margin-top: -5px;
+        border-width: 5px;
+        border-style: solid;
+        border-color: transparent $unnic-color-neutral-black transparent transparent;
+      }
+    }
+    &-left{
+      top: -5px;
+      right: 110%;
+
+      &::after {
+        content: "";
+        position: absolute;
+        top: 50%;
+        left: 100%;
+        margin-top: -5px;
+        border-width: 5px;
+        border-style: solid;
+        border-color: transparent transparent transparent $unnic-color-neutral-black;
+      }
+    }
+}
+
+.unnic-tooltip:hover {
+  .unnic-tooltip-label{
+      visibility: visible;
+    }
+}
+
+</style>
