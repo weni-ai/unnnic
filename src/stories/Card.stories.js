9a21d090e4127f6ed3fccff1494d67a20172d8bf
ricardovasconcelos <ricardovasconcelos.dev@gmail.com>
2021-03-15 14:27:34 -0300

    fix: added option  default to cards type

--- src/stories/Card.stories.js
+++ src/stories/Card.stories.js
@@ -7,7 +7,7 @@ export default {
   argTypes: {
     enabled: { control: { type: 'select', options: [true, false] } },
     infoPosition: { control: { type: 'select', options: ['top', 'right', 'bottom', 'left'] } },
-    type: { control: { type: 'select', options: ['title', 'dash', 'account', 'status'] } },
+    type: { control: { type: 'select', options: ['title', 'dash', 'account', 'status', 'default'] } },
     icon: { control: { type: 'select', options: iconList } },
     scheme: {
       control: {
