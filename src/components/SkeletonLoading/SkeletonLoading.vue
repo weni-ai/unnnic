7a15548d53f31c5b2228e89f5eb4b01975f62e7f
Matheus Martins Sarmento <matheusms280100@gmail.com>
2021-05-04 15:57:56 -0300

    feat(skeleton) Add skeleton loading component

---
+++ src/components/SkeletonLoading/SkeletonLoading.vue
@@ -0,0 +1,107 @@
+<script>
+
+import { SkeletonStyle } from './skeletonTheme.vue';
+const isEmptyVNode = (children) => {
+  if (!children) return true;
+  const [firstNode] = children;
+  let str = firstNode.text;
+  if (str) {
+    // remove all line-break and space character
+    str = str.replace(/(\n|\r\n|\s)/g, '');
+  }
+  return typeof firstNode.tag === 'undefined' && !str;
+};
+export default {
+  name: 'unnnicSkeleton',
+  inject: {
+    themeStyle: {
+      from: '_themeStyle',
+      default: SkeletonStyle
+    },
+    theme: {
+      from: '_skeletonTheme',
+      default: ({})
+    }
+  },
+  props: {
+    prefix: {
+      type: String,
+      default: 'unnnic'
+    },
+    count: {
+      type: Number,
+      default: 1
+    },
+    duration: {
+      type: Number,
+      default: 1.5
+    },
+    tag: {
+      type: String,
+      default: 'span'
+    },
+    width: [String, Number],
+    height: [String, Number],
+    circle: Boolean,
+    loading: undefined
+  },
+  computed: {
+    isLoading() {
+      return typeof this.theme.loading !== 'undefined' ? this.theme.loading : this.loading;
+    }
+  },
+  render(h) {
+    const {
+      width, height, duration, prefix, circle, count, tag, isLoading
+    } = this;
+    const classes = [`${prefix}-skeleton`];
+    const elements = [];
+    const styles = { ...this.themeStyle };
+    if (duration) {
+      styles.animation = `SkeletonLoading ${duration}s ease-in-out infinite`;
+    } else {
+      styles.backgroundImage = '';
+    }
+    if (width) styles.width = width;
+    if (height) styles.height = height;
+    if (circle) styles.borderRadius = '50%';
+    for (let i = 0; i < count; i += 1) {
+      elements.push(
+        <span
+          key={i}
+          class={classes}
+          style={styles}>
+          &zwnj;
+        </span>
+      );
+    }
+    const showLoading = typeof isLoading !== 'undefined' ? isLoading : isEmptyVNode(this.$slots.default);
+    if (tag) {
+      return h(tag, !showLoading ? this.$slots.default : elements);
+    }
+    return (!showLoading ? this.$slots.default : <span>{ elements }</span>);
+  }
+};
+</script>
+
+<style lang="scss">
+@import "../../assets/scss/unnnic.scss";
+
+.unnnic-skeleton {
+  background-size: 500px 100%;
+  background-repeat: no-repeat;
+  border-radius: $unnnic-border-radius-sm;
+  display: inline-block;
+  line-height: 1;
+  width: 100%;
+  height: inherit;
+}
+@keyframes SkeletonLoading {
+  0% {
+    background-position: -500px 0;
+  }
+  100% {
+    background-position: calc(500px + 100%) 0;
+  }
+}
+</style>
