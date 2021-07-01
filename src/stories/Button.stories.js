d4f19e69a6e067314b587508190361a0add69c74
paulobernardo <paulo.bernardo@ilhasoft.com.br>
2021-07-01 11:52:33 -0300

    feat: color schemes for primary buttons

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -12,6 +12,7 @@ export default {
     iconRight: { control: { type: 'select', options: [null, ...iconList] } },
     disabled: { control: { type: 'boolean' } },
     loading: { control: { type: 'boolean' } },
+    scheme: { control: { type: 'select', options: ['feedback-red', 'feedback-green'] } },
   },
 };
 
