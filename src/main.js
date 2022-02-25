624c992a40652fbf8976980c77d78550a9416663
paulobernardo <paulo.bernardo@weni.ai>
2022-02-25 16:41:49 -0300

    i18n

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import vClickOutside from 'v-click-outside';
+import i18n from './utils/plugins/i18n';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
@@ -7,5 +8,6 @@ Vue.config.productionTip = false;
 Vue.use(vClickOutside);
 
 new Vue({
+  i18n,
   render: (h) => h(App),
 }).$mount('#app');
