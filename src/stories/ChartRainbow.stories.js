1fc9c0cdca7247dcdc09772b1bec879c9f6140d5
Matheus Cristian <mathues@email.com>
2022-05-09 10:46:02 -0300

    feat: added chart rainbow component

---
+++ src/stories/ChartRainbow.stories.js
@@ -0,0 +1,34 @@
+import unnnicChartRainbow from '../components/ChartRainbow/ChartRainbow.vue';
+
+export default {
+  title: 'example/ChartRainbow',
+  component: unnnicChartRainbow,
+  argTypes: {},
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+
+  components: {
+    unnnicChartRainbow,
+  },
+
+  data() {
+    return {};
+  },
+
+  template: `
+    <div>
+      <unnnic-chart-rainbow v-bind="$props" />
+    </div>
+  `,
+
+  methods: {},
+});
+
+export const Default = Template.bind({});
+
+Default.args = {
+  value: 50,
+  description: 'Description',
+};
