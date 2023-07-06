cba2ee50383d877f1475f920fc61b2fa0dad46ea
Aldemylla <aldemylla@gmail.com>
2023-07-06 18:38:46 -0300

    feat: Converted Tooltip component to Vue 3

---
+++ src/stories/ToolTip.stories.js
@@ -0,0 +1,22 @@
+import { unnnicFontSize } from '../components/config';
+import unnnicToolTip from '../components/ToolTip/ToolTip.vue';
+
+export default {
+  title: 'Example/ToolTip',
+  component: unnnicToolTip,
+  argTypes: {
+    text: { control: { type: 'text' } },
+    side: { control: { type: 'select', options: ['top', 'right', 'bottom', 'left'] } },
+    maxWidth: { control: { type: 'text' } },
+    forceOpen: { control: { type: 'boolean' } },
+  },
+};
+
+export const Normal = {
+  args: {
+    text: 'Label',
+    enabled: true,
+    maxWidth: `${15 * unnnicFontSize}px`,
+    side: 'bottom',
+  }
+}
