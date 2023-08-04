10e7fb45ec72ec20539f1eeb68e197be2e99be9a
Aldemylla <aldemylla@gmail.com>
2023-08-04 09:49:25 -0300

    feat: Migrated CardNumber to Vue 3

---
+++ src/stories/CardNumber.stories.js
@@ -0,0 +1,14 @@
+import unnnicCardNumber from '../components/CardNumber/CardNumber.vue';
+
+export default {
+  title: 'Card/CardNumber',
+  component: unnnicCardNumber,
+  argTypes: {},
+};
+
+export const Default = {
+  args: {
+    description: 'Description',
+    number: '00',
+  },
+};
