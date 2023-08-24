5e870c4a5ccf0dd4e6369dc1dd77be1c4a9d5988
Matheus Cristian <mathues@email.com>
2023-08-24 16:52:11 -0300

    feat: added pluralization and variables support to unnnic i18n

--- src/mixins/i18n.js
+++ src/mixins/i18n.js
@@ -14,11 +14,34 @@ export default {
     i18n(key, defaults) {
       const locale = (this.locale || get(this, '$i18n.locale'))?.toLowerCase();
 
-      const text = get(this.translations, `${key}.${locale}`, get(this.translations, key));
+      let text = get(this.translations, `${key}.${locale}`, get(this.translations, key));
 
       if (!text) {
-        return get(get(this.defaultTranslations, key), locale, defaults);
+        text = get(get(this.defaultTranslations, key), locale, defaults);
       }
+
+      if (text?.includes('|') && typeof arguments[1] === 'number') {
+        const count = arguments[1];
+        const pluralization = text.split('|');
+
+        if (pluralization.length === 3) {
+          text = pluralization[count] || pluralization[2];
+        } else if (pluralization.length === 2) {
+          text = count === 1 ? pluralization[0] : pluralization[1];
+        }
+      }
+
+      let vars = {};
+
+      Object.values(arguments).slice(1).forEach((argument) => {
+        if (!(argument instanceof Array) && argument instanceof Object) {
+          vars = argument;
+        }
+      });
+
+      Object.keys(vars).forEach((varName) => {
+        text = text.replaceAll(new RegExp(`{ *${varName} *}`, 'g'), vars[varName]);
+      });
 
       return text;
     },
