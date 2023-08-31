5acc6da39d54f6aea49af3e717c595452098f64c
aldemylla <aldemylla@gmail.com>
2023-08-31 19:00:42 -0300

    feat: ChatsMessage audio and image medias

--- src/components/ChatsMessage/ChatsMessage.vue
+++ src/components/ChatsMessage/ChatsMessage.vue
@@ -1,21 +1,44 @@
 <template>
-  <div class="unnnic-chats-message" :class="{ sent: type === 'sent', sending }">
-    <p class="unnnic-chats-message__text">{{ message }}</p>
-    <p class="unnnic-chats-message__time">{{ treatedTime }}</p>
+  <div
+    class="unnnic-chats-message"
+    :class="{
+      sent: type === 'sent',
+      sending: status === 'sending',
+      'is-media': isMedia,
+      'is-image': isImage,
+    }"
+  >
+    <p class="unnnic-chats-message__text" v-if="this.$slots.text">
+      <slot name="text" :content="formattedText" />
+    </p>
+    <div
+      class="unnnic-chats-message__media__container"
+      :class="{ failed: failedToSendMedia }"
+      @click="$emit('click-image')"
+    >
+      <slot name="media" />
+      <unnnic-chats-message-status-backdrop
+        v-if="(sendingMedia || failedToSendMedia) && isImage"
+        :status="status"
+      />
+    </div>
+
+    <unnnic-icon v-if="sendingMedia" icon="loading-circle-1" size="avatar-nano" />
+    <p v-else class="unnnic-chats-message__time">{{ formattedTime }}</p>
   </div>
 </template>
 
 <script>
+import UnnnicChatsMessageStatusBackdrop from './ChatsMessageStatusBackdrop.vue';
+import UnnnicIcon from '../Icon.vue';
+
 export default {
   name: 'UnnnicChatsMessage',
+  components: { UnnnicChatsMessageStatusBackdrop, UnnnicIcon },
   props: {
-    message: {
-      type: String,
-      default: '',
-    },
     type: {
       type: String,
-      default: '',
+      default: 'received',
       validate(type) {
         return ['received', 'sent'].includes(type);
       },
@@ -29,10 +52,21 @@ export default {
       type: Boolean,
       default: false,
     },
+    status: {
+      type: String,
+      default: 'sent',
+      validate(status) {
+        return ['sending', 'sent', 'failed'].includes(status);
+      },
+    },
   },
 
   computed: {
-    treatedTime() {
+    formattedText() {
+      const slotContent = this.$slots.text?.[0]?.text;
+      return typeof slotContent === 'string' ? slotContent : '';
+    },
+    formattedTime() {
       const date = new Date(this.time);
       const hours = date.getHours();
       const minutes = date.getMinutes();
@@ -40,12 +74,32 @@ export default {
       const formattedTime = `${hours}:${minutes}`;
       return formattedTime;
     },
+    isMedia() {
+      return !!this.$slots.media;
+    },
+    isImage() {
+      const slotContents = this.$slots.media;
+
+      if (slotContents && slotContents[0]) {
+        const isImgTag = slotContents[0].tag === 'img';
+        return isImgTag;
+      }
+      return false;
+    },
+    sendingMedia() {
+      return this.isMedia && this.status === 'sending';
+    },
+    failedToSendMedia() {
+      return (this.isImage || this.isVideo) && this.status === 'failed';
+    },
   },
 };
 </script>
 
 <style lang="scss">
 @import '../../assets/scss/unnnic.scss';
+
+$defaultLineHeight: $unnnic-font-size-body-gt + $unnnic-line-height-medium;
 
 .unnnic-chats-message {
   width: fit-content;
@@ -57,7 +111,7 @@ export default {
   justify-content: space-between;
   gap: $unnnic-spacing-xs;
 
-  border-radius: $unnnic-border-radius-lg;
+  border-radius: $unnnic-border-radius-md;
 
   padding: $unnnic-spacing-xs;
 
@@ -79,16 +133,48 @@ export default {
     }
   }
 
+  &.is-media {
+    padding: $unnnic-spacing-nano $unnnic-spacing-xs;
+
+    &.is-image {
+      display: grid;
+      justify-items: end;
+
+      overflow: hidden;
+
+      img {
+        width: 200px;
+        height: auto;
+        max-width: 200px;
+        min-height: 200px;
+        max-height: 300px;
+
+        object-fit: cover;
+      }
+    }
+  }
+
   &__text {
     padding: $unnnic-spacing-nano 0;
 
     color: $unnnic-color-neutral-dark;
-    line-height: $unnnic-font-size-body-gt + $unnnic-line-height-medium;
+    line-height: $defaultLineHeight;
     word-break: break-word;
   }
 
   &__time {
     color: $unnnic-color-neutral-clean;
+    line-height: $defaultLineHeight;
+  }
+
+  &__media__container {
+    position: relative;
+
+    display: flex;
+
+    &.failed {
+      cursor: pointer;
+    }
   }
 }
 </style>
--- src/stories/ChatsMessage.stories.js
+++ src/stories/ChatsMessage.stories.js
@@ -1,5 +1,9 @@
-/* eslint-disable no-alert */
 import unnnicChatsMessage from '../components/ChatsMessage/ChatsMessage.vue';
+import unnnicAudioRecorder from '../components/AudioRecorder/AudioRecorder.vue';
+import AudioSample from './assets/audios/audio-recorder-sample.mp3';
+import ImageSample1 from './assets/images/un.png';
+import ImageSample2 from './assets/images/unnnic.png';
+import ImageSample3 from './assets/images/unnnicPortrait.png';
 
 export default {
   title: 'Chats/Message',
@@ -9,7 +13,14 @@ export default {
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { unnnicChatsMessage },
-  template: '<unnnic-chats-message v-bind="$props"/>',
+  template: `
+    <unnnic-chats-message v-bind="$props">
+      <template #text>
+        Interdum et malesuada fames ac ante ipsum primis in faucibus.
+        Curabitur facilisis congue sagittis.
+      </template>
+    </unnnic-chats-message>
+  `,
 });
 
 const ReceivedAndSentTemplate = (args, { argTypes }) => ({
@@ -19,8 +30,18 @@ const ReceivedAndSentTemplate = (args, { argTypes }) => ({
     <div style="
       display: flex;
     ">
-      <unnnic-chats-message v-bind="$props" />
-      <unnnic-chats-message type="sent" v-bind="$props" />
+      <unnnic-chats-message v-bind="$props">
+        <template #text>
+          Interdum et malesuada fames ac ante ipsum primis in faucibus.
+          Curabitur facilisis congue sagittis.
+        </template>
+      </unnnic-chats-message>
+      <unnnic-chats-message type="sent" v-bind="$props">
+        <template #text>
+          Interdum et malesuada fames ac ante ipsum primis in faucibus.
+          Curabitur facilisis congue sagittis.
+        </template>
+      </unnnic-chats-message>
     </div>
   `,
 });
@@ -33,38 +54,124 @@ const MultipleMessagesTemplate = (args, { argTypes }) => ({
       display: grid;
       gap: 16px;
     ">
-      <unnnic-chats-message message="Ita." v-bind="$props" />
-      <unnnic-chats-message message="Pashca ovum 😯😉" v-bind="$props" />
-      <unnnic-chats-message message="Fusce scelerisque odio sit amet eleifend." v-bind="$props" />
-      <unnnic-chats-message message="In mattis tristique massa, ut ornare urna porttitor faucibus. Pellentesque habitant morbi." v-bind="$props" />
-      <unnnic-chats-message message="Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec dignissim sapien ac condimentum sagittis. Cras vitae consectetur risus." v-bind="$props" />
-      <unnnic-chats-message message="Aenean vel pharetra ligula. Donec sit amet porttitor dui. Suspendisse lacinia mauris egestas, aliquam dui non, tristique augue. Donec at pulvinar leo, eget lobortis turpis. Ut hendrerit varius tortor, id mollis justo dictum eget. Praesent finibus urna vel lacus pretium consequat. Proin imperdiet ante tempus feugiat tristique. Duis in ultrices sem. Cras pharetra eleifend ligula vel commodo. Phasellus placerat faucibus est, nec posuere est venenatis sed. Phasellus a quam felis. Fusce volutpat sem eget urna sagittis laoreet non maximus quam. Mauris porta posuere commodo." v-bind="$props" />
+      <unnnic-chats-message v-bind="$props">
+        <template #text>Ita.</template>
+      </unnnic-chats-message>
+      <unnnic-chats-message v-bind="$props">
+        <template #text>Pashca ovum 😯😉</template>
+      </unnnic-chats-message>
+      <unnnic-chats-message v-bind="$props">
+        <template #text>Fusce scelerisque odio sit amet eleifend.</template>
+      </unnnic-chats-message>
+      <unnnic-chats-message v-bind="$props">
+        <template #text>
+          In mattis tristique massa, ornare urna porttitor faucibus. Pellentesque habitant morbi.
+        </template>
+      </unnnic-chats-message>
+      <unnnic-chats-message v-bind="$props">
+        <template #text>
+          Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          Donec dignissim sapien ac condimentum sagittis. Cras vitae consectetur risus.
+        </template>
+      </unnnic-chats-message>
+      <unnnic-chats-message v-bind="$props">
+        <template #text>
+          Aenean vel pharetra ligula. Donec sit amet porttitor dui.
+          Suspendisse lacinia mauris egestas, aliquam dui non, tristique augue. Donec at pulvinar
+          leo, eget lobortis turpis. Ut hendrerit varius tortor, id mollis justo dictum eget.
+          Praesent finibus urna vel lacus pretium consequat. Proin imperdiet ante tempus feugiat
+          tristique. Duis in ultrices sem. Cras pharetra eleifend ligula vel commodo.
+          Phasellus placerat faucibus est, nec posuere est venenatis sed. Phasellus a quam felis.
+          Fusce volutpat sem eget urna sagittis laoreet non maximus quam. Mauris porta
+          posuere commodo.
+        </template>
+      </unnnic-chats-message>
     </div>
   `,
 });
 
+const MediaTemplate = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { unnnicChatsMessage, unnnicAudioRecorder },
+  data() {
+    return {
+      audio: AudioSample,
+      image1: ImageSample1,
+      image2: ImageSample2,
+      image3: ImageSample3,
+    };
+  },
+  template: `
+  <div style="
+    display: grid;
+    gap: 16px;
+  ">
+    <unnnic-chats-message v-bind="$props">
+      <template #media>
+        <unnnic-audio-recorder style="padding: 8px; margin: 4px 0;" ref="audio-recorder" :src="audio" :canDiscard="false" />
+      </template>
+    </unnnic-chats-message>
+    <div  style="
+      display: flex;
+      gap: 16px;
+    ">
+      <unnnic-chats-message v-bind="$props">
+        <template #media>
+          <img :src="image1" />
+        </template>
+      </unnnic-chats-message>
+      <unnnic-chats-message  status="sending" v-bind="$props">
+        <template #media>
+          <img :src="image1" />
+        </template>
+      </unnnic-chats-message>
+      <unnnic-chats-message  status="failed" v-bind="$props">
+        <template #media>
+          <img :src="image1" />
+        </template>
+      </unnnic-chats-message>
+    </div>
+    <unnnic-chats-message v-bind="$props">
+      <template #media>
+        <img :src="image2" />
+      </template>
+    </unnnic-chats-message>
+    <unnnic-chats-message v-bind="$props">
+      <template #media>
+        <img :src="image3" />
+      </template>
+    </unnnic-chats-message>
+  </div>
+  `,
+});
+
+const defaultArgs = { time: new Date('2023-08-08T09:54:07.876230-03:00') };
+
 export const Default = Template.bind({});
 Default.args = {
-  message: 'Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur facilisis congue sagittis. ',
-  time: new Date('2023-08-08T09:54:07.876230-03:00'),
+  ...defaultArgs,
 };
 
 export const Sending = Template.bind({});
 Sending.args = {
-  message: 'Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur facilisis congue sagittis. ',
-  sending: true,
+  ...defaultArgs,
+  status: 'sending',
   type: 'sent',
-  time: new Date('2023-08-08T09:54:07.876230-03:00'),
 };
 
 export const ReceivedAndSent = ReceivedAndSentTemplate.bind({});
 ReceivedAndSent.args = {
-  message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam rhoncus imperdiet enim, ac mattis turpis ultricies eget. Phasellus neque sapien, tincidunt nec scelerisque eget, lacinia non tellus.',
-  time: new Date('2023-08-08T09:54:07.876230-03:00'),
+  ...defaultArgs,
 };
 
 export const MultipleMessages = MultipleMessagesTemplate.bind({});
 MultipleMessages.args = {
+  ...defaultArgs,
   type: 'sent',
-  time: new Date('2023-08-08T09:54:07.876230-03:00'),
+};
+
+export const Media = MediaTemplate.bind({});
+Media.args = {
+  ...defaultArgs,
+  type: 'sent',
 };
