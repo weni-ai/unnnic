d366a1f00d509351d932a78a29e04212ae597179
aldemylla <aldemylla@gmail.com>
2023-10-18 10:33:35 -0300

    fix: ChatsHeader crumb click return crumb clicked

--- src/components/ChatsHeader/ChatsHeader.vue
+++ src/components/ChatsHeader/ChatsHeader.vue
@@ -2,7 +2,7 @@
   <div class="unnnic-chats-header__container">
     <header class="unnnic-chats-header" :class="{ contact: !!avatarName }">
       <div class="unnnic-chats-header__topbar" v-if="!avatarName">
-        <unnnic-breadcrumb :crumbs="crumbs" @crumbClick="$emit('crumbClick')"/>
+        <unnnic-breadcrumb :crumbs="crumbs" @crumbClick="(crumb) => $emit('crumbClick', crumb)"/>
         <unnnic-button-close @close="close" size="ant" />
       </div>
       <main class="unnnic-chats-header__main">
