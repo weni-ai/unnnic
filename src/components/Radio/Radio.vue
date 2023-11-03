ad51dbff8efb2a59a37e7e87686278c906a6acac
Paulo Bernardo <paulo.bernardo@weni.ai>
2023-11-03 15:32:23 -0300

    feat: migrate radio into vue3

---
+++ src/components/Radio/Radio.vue
@@ -0,0 +1,106 @@
+<template>
+  <div
+    @click="click"
+    :class="[
+      'unnnic-radio-container',
+      `unnnic-radio-container--${size}`,
+      disabled ? 'disabled' : null,
+    ]"
+  >
+    <unnnic-icon-svg class="unnnic-radio" :icon="icon" :scheme="color" :size="size" />
+
+    <span class="label">
+      <slot />
+    </span>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import unnnicIconSvg from '../Icon.vue';
+
+const props = defineProps({
+  modelValue: {
+    type: [String, Number],
+  },
+  value: {
+    type: [String, Number],
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  size: {
+    type: String,
+    default: 'md',
+    validator(value) {
+      return ['md', 'sm'].includes(value);
+    },
+  },
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const valueName = computed(() => (props.modelValue === props.value ? 'selected' : 'default'));
+const icon = computed(
+  () =>
+    ({
+      selected: 'radio-selected',
+      default: props.disabled ? 'radio-disable' : 'radio-default',
+    }[valueName.value]),
+);
+const color = computed(() => {
+  if (props.disabled) {
+    return 'brand-sec';
+  }
+
+  return valueName.value === 'selected' ? 'brand-weni' : 'neutral-soft';
+});
+
+function click() {
+  if (!props.disabled) {
+    emit('update:modelValue', props.value);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/unnnic.scss';
+
+.unnnic-radio-container {
+  display: inline-flex;
+  align-items: center;
+
+  &:not(.disabled) {
+    cursor: pointer;
+  }
+
+  &--sm .unnnic-radio {
+    margin: $unnnic-spacing-stack-nano $unnnic-spacing-inline-nano;
+  }
+
+  &--md .unnnic-radio {
+    margin: $unnnic-spacing-stack-nano 0;
+    margin-left: $unnnic-spacing-inline-nano;
+    margin-right: $unnnic-spacing-inline-xs;
+  }
+
+  .label {
+    font-family: $unnnic-font-family-secondary;
+    font-weight: $unnnic-font-weight-regular;
+    color: $unnnic-color-neutral-darkest;
+    margin: $unnnic-spacing-stack-nano 0;
+    margin-right: $unnnic-spacing-inline-nano;
+  }
+
+  &--sm .label {
+    font-size: $unnnic-font-size-body-md;
+    line-height: $unnnic-font-size-body-md + $unnnic-line-height-md;
+  }
+
+  &--md .label {
+    font-size: $unnnic-font-size-body-gt;
+    line-height: $unnnic-font-size-body-gt + $unnnic-line-height-md;
+  }
+}
+</style>
