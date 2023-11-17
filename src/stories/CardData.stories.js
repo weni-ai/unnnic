7da58c7478b667d38ed9c21eb53f77e529d61a19
Paulo Bernardo <paulo.bernardo@weni.ai>
2023-11-17 15:27:59 -0300

    feat: migrate cardData into vue3

---
+++ src/stories/CardData.stories.js
@@ -0,0 +1,41 @@
+import unnnicCardData from '../components/Card/CardData.vue';
+
+export default {
+  title: 'Card/CardData',
+  component: unnnicCardData,
+  argTypes: {
+    title: { control: { type: 'text' } },
+    description: { control: { type: 'text' } },
+    score: { control: { type: 'text' } },
+    info: { control: { type: 'text' } },
+    checked: { control: { type: 'boolean' } },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+
+  components: {
+    unnnicCardData,
+  },
+
+  data() {
+    return {};
+  },
+
+  template: `
+    <div>
+      <unnnic-card-data v-bind="$props"/>
+    </div>
+  `,
+});
+
+export const Default = Template.bind({});
+
+Default.args = {
+  title: 'Title Card',
+  description: 'Description Description Description Description Description Description Description Description.',
+  score: '4.9',
+  info: '(3 comments)',
+  checked: true,
+};
