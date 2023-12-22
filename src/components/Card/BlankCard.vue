b2fa0195db1bfddf08946328ff8d6ed12f2ed79f
aldemylla <aldemylla@gmail.com>
2023-12-22 15:54:22 -0300

    feat: Migrated BlankCard

---
+++ src/components/Card/BlankCard.vue
@@ -0,0 +1,83 @@
+<template>
+  <div :class="{
+      'unnnic-card-blank': true,
+      'unnnic-card-blank__clickable': clickable,
+    }">
+    <div class="unnnic-card-blank__content">
+        <unnnic-icon-svg
+          :icon="icon"
+          scheme="neutral-clean"
+          class="unnnic-card-blank__content__icon"
+          size="xl"
+        />
+
+      <div class="unnnic-card-blank__content__text">{{ text }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import UnnnicIconSvg from '../Icon.vue';
+
+export default {
+  name: 'unnnic-card',
+  components: {
+    UnnnicIconSvg,
+  },
+  props: {
+    text: {
+      type: String,
+      default: null,
+    },
+    icon: {
+      type: String,
+      default: null,
+    },
+    clickable: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../../assets/scss/unnnic.scss";
+
+.unnnic-card-blank {
+  display: flex;
+  background-color: $unnnic-color-background-snow;
+  border-radius: $unnnic-border-radius-md;
+  padding: $unnnic-inset-md;
+  border: $unnnic-border-width-thinner solid $unnnic-color-neutral-soft;
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: $unnnic-shadow-level-near;
+  }
+
+  &__clickable {
+    cursor: pointer;
+  }
+
+  &__content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    width: 100%;
+
+    &__icon {
+      padding-bottom: $unnnic-spacing-stack-xs;
+    }
+
+    &__text {
+      font-family: $unnnic-font-family-secondary;
+      font-size: $unnnic-font-size-body-lg;
+      font-weight: $unnnic-font-weight-regular;
+      color: $unnnic-color-neutral-cloudy;
+      line-height: $unnnic-font-size-body-lg + $unnnic-line-height-medium;
+    }
+  }
+}
+</style>
