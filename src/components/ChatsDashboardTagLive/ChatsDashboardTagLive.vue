6482a1006a8546344bf090c33ddf3f6d7c0ecae1
aldemylla <aldemylla@gmail.com>
2023-12-22 21:00:10 -0300

    feat: Migrated ChatsDashboardTagLive

---
+++ src/components/ChatsDashboardTagLive/ChatsDashboardTagLive.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="unnnic-chats-dashboard-tag-live">
+    <unnnic-icon
+      class="unnnic-chats-dashboard-tag-live__indicator"
+      icon="indicator"
+      scheme="aux-green-300"
+    />
+    <p class="unnnic-chats-dashboard-tag-live__text">{{ i18n('live') }}</p>
+  </div>
+</template>
+
+<script>
+import UnnnicI18n from '../../mixins/i18n';
+
+import UnnnicIcon from '../Icon.vue';
+
+export default {
+  name: 'UnnnicChatsDashboardTagLive',
+
+  mixins: [UnnnicI18n],
+
+  components: {
+    UnnnicIcon,
+  },
+
+  data() {
+    return {
+      defaultTranslations: {
+        live: {
+          'pt-br': 'Ao vivo',
+          en: 'Live',
+          es: 'En vivo',
+        },
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../../assets/scss/unnnic.scss';
+
+.unnnic-chats-dashboard-tag-live {
+  padding: $unnnic-spacing-ant;
+
+  display: inline-flex;
+  align-items: center;
+  gap: $unnnic-spacing-ant;
+
+  border-radius: $unnnic-border-radius-sm;
+  background-color: $unnnic-color-neutral-lightest;
+
+  color: $unnnic-color-neutral-dark;
+  font-family: $unnnic-font-family-secondary;
+
+  &__indicator {
+    .primary {
+      animation: ease-in-out pulse 2s infinite;
+    }
+
+    @keyframes pulse {
+      0% {
+        fill: $unnnic-color-neutral-soft;
+      }
+
+      50% {
+        fill: $unnnic-color-aux-green-300;
+      }
+
+      100% {
+        fill: $unnnic-color-neutral-soft;
+      }
+    }
+  }
+
+    &__text {
+      margin: 0;
+
+      font-size: $unnnic-font-size-body-gt;
+    }
+}
+</style>
