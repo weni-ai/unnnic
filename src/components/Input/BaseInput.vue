cf11dc86883043cb5c85966958475d686ee0ca44
Matheus Cristian <mathues@email.com>
2023-12-28 13:02:53 -0300

    feat: Migrated Input, TextInput and BaseInput components

---
+++ src/components/Input/BaseInput.vue
@@ -0,0 +1,139 @@
+<template>
+  <input
+    v-if="mask"
+    v-bind="attributes"
+    :value="modelValue"
+    :class="classes"
+    :type="nativeType"
+    v-mask="mask"
+  />
+
+  <input
+    v-else
+    v-bind="attributes"
+    :value="modelValue"
+    :class="classes"
+    :type="nativeType"
+  />
+</template>
+
+<script>
+import { mask } from 'vue-the-mask';
+
+export default {
+  data() {
+    return {};
+  },
+  directives: { mask },
+  props: {
+    type: {
+      type: String,
+      default: 'normal',
+      validator(value) {
+        return ['normal', 'error'].indexOf(value) !== -1;
+      },
+    },
+    modelValue: {
+      type: String,
+      default: '',
+    },
+    nativeType: {
+      type: String,
+      default: 'text',
+    },
+    size: {
+      type: String,
+      default: 'md',
+    },
+    mask: {
+      type: [String, Array],
+      default: '',
+    },
+  },
+  computed: {
+    attributes() {
+      return {
+        ...this.$attrs,
+        ...this.$attrs['v-bind'],
+        onInput: (event) => {
+          this.$emit('update:model-value', event.srcElement.value);
+        },
+      };
+    },
+
+    classes() {
+      return [
+        'input',
+        `size-${this.size}`,
+        this.type,
+      ];
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/unnnic.scss';
+
+.input {
+  background: $unnnic-color-neutral-snow;
+  border: none;
+  outline-style: solid;
+  outline-color: $unnnic-color-neutral-soft;
+  outline-width: $unnnic-border-width-thinner;
+  outline-offset: -$unnnic-border-width-thinner;
+  border-radius: $unnnic-border-radius-sm;
+  color: $unnnic-color-neutral-dark;
+  caret-color: $unnnic-color-neutral-clean;
+  font-weight: $unnnic-font-weight-regular;
+  font-family: $unnnic-font-family-secondary;
+  box-sizing: border-box;
+  width: 100%;
+
+  &.size {
+    &-md {
+      font-size: $unnnic-font-size-body-gt;
+      line-height: $unnnic-font-size-body-gt + $unnnic-line-height-medium;
+      padding: $unnnic-squish-xs;
+    }
+
+    &-sm {
+      font-size: $unnnic-font-size-body-md;
+      line-height: $unnnic-font-size-body-md + $unnnic-line-height-medium;
+      padding: $unnnic-squish-nano;
+    }
+  }
+
+  &:focus {
+    outline-color: $unnnic-color-neutral-clean;
+  }
+
+  &.error {
+    outline-color: $unnnic-color-feedback-red;
+    color: $unnnic-color-feedback-red;
+  }
+
+  &::placeholder {
+    color: $unnnic-color-neutral-cleanest;
+    opacity: 1; /* Firefox */
+  }
+
+  &:-ms-input-placeholder,
+  &::-ms-input-placeholder {
+    /* Internet Explorer 10-11 */
+    color: $unnnic-color-brand-sec;
+  }
+
+  &:disabled {
+    outline-color: $unnnic-color-neutral-cleanest;
+    background-color: $unnnic-color-neutral-lightest;
+    cursor: not-allowed;
+    color: $unnnic-color-neutral-cleanest;
+
+    &::placeholder {
+      color: $unnnic-color-neutral-cleanest;
+      opacity: 1; /* Firefox */
+    }
+  }
+}
+</style>
---
+++ src/stories/Input.stories.js
@@ -0,0 +1,68 @@
+import unnnicInput from '../components/Input/Input.vue';
+import iconList from '../utils/iconList';
+
+export default {
+  title: 'Form/Input',
+  component: unnnicInput,
+  argTypes: {
+    label: { control: { type: 'text' } },
+    size: { control: { type: 'select' }, options: ['md', 'sm'] },
+    type: { control: { type: 'select' }, options: ['normal', 'error'] },
+    nativeType: { control: { type: 'select' }, options: ['text', 'password'] },
+    placeholder: { control: { type: 'text' } },
+    iconLeft: { control: { type: 'select' }, options: iconList },
+    iconRight: { control: { type: 'select' }, options: iconList },
+    allowTogglePassword: { control: { type: 'boolean' } },
+    hasCloudyColor: { control: { type: 'select' }, options: [true, false] },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { unnnicInput },
+  data() {
+    return {
+      v: '',
+    };
+  },
+  template: '<div><pre>v-model: {{ v }}</pre><unnnic-input v-bind="$props" v-model="v" /></div>',
+});
+
+export const Normal = Template.bind({});
+Normal.args = {
+  type: 'normal',
+  placeholder: 'Placeholder',
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  type: 'error',
+  placeholder: 'Placeholder',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+  placeholder: 'Placeholder',
+};
+
+export const Password = Template.bind({});
+Password.args = {
+  placeholder: 'Password',
+  nativeType: 'password',
+  allowTogglePassword: true,
+};
+
+export const IconLeftAndRight = Template.bind({});
+IconLeftAndRight.args = {
+  placeholder: 'Text',
+  iconLeft: 'read-email-at-1',
+  iconRight: 'read-email-at-1',
+};
+
+export const Mask = Template.bind({});
+Mask.args = {
+  label: 'CPF or CNPJ',
+  placeholder: 'Text',
+  mask: ['###.###.###-##', '##.###.###/####-##'],
+};
