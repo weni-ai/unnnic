12675bfcb7486ecdc64a08f54f86d3904019b617
Matheus Cristian <mathues@email.com>
2023-12-28 14:34:06 -0300

    feat: Migrated Carousel

---
+++ src/components/Carousel/Carousel.vue
@@ -0,0 +1,62 @@
+<template>
+    <component
+      class="unnnic-carousel-component"
+      :is="currentComponent"
+      :v-bind="$attrs"
+      :name="name"
+      :clickable="clickable"
+      :tagItems="tagItems"
+      @selected="$emit('update:model-value', $event)"
+      :model-value="modelValue"
+    />
+</template>
+
+<script>
+import TagCarousel from './TagCarousel.vue';
+
+export default {
+  model: {
+    event: 'update:model-value',
+  },
+
+  name: 'unnnic-carousel',
+  props: {
+    modelValue: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+    type: {
+      type: String,
+      default: 'brand',
+      validator(value) {
+        return (
+          ['brand'].indexOf(value) !== -1
+        );
+      },
+    },
+    tagItems: {
+      type: Array,
+      default: () => [],
+    },
+    name: {
+      type: String,
+      default: null,
+    },
+    icon: {
+      type: String,
+      default: null,
+    },
+    clickable: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    currentComponent() {
+      return TagCarousel;
+    },
+  },
+};
+</script>
