9b1cbb5e7b112ec66cd7d80b1aa4b6b7c222e0c0
Matheus Cristian <mathues@email.com>
2024-01-26 10:58:02 -0300

    feat: added attention type button variation

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -5,50 +5,88 @@ export default {
   title: 'Form/Button',
   component: unnnicButton,
   argTypes: {
-    type: { control: { type: 'select', options: ['primary', 'secondary', 'tertiary', 'alternative', 'warning'] } },
-    size: { control: { type: 'select', options: ['large', 'small'] } },
+    type: {
+      control: { type: 'select', },
+      options: ['primary', 'secondary', 'tertiary', 'alternative', 'warning', 'attention'],
+    },
+    size: {
+      control: { type: 'select', },
+      options: ['large', 'small'],
+    },
     text: { control: { type: 'text' } },
-    iconLeft: { control: { type: 'select', options: [null, ...iconList] } },
-    iconCenter: { control: { type: 'select', options: [null, ...iconList] } },
-    iconRight: { control: { type: 'select', options: [null, ...iconList] } },
+    iconLeft: {
+      control: { type: 'select', },
+      options: [null, ...iconList],
+    },
+    iconCenter: {
+      control: { type: 'select', },
+      options: [null, ...iconList],
+    },
+    iconRight: {
+      control: { type: 'select', },
+      options: [null, ...iconList],
+    },
     disabled: { control: { type: 'boolean' } },
     loading: { control: { type: 'boolean' } },
   },
 };
 
-const Template = (args, { argTypes }) => ({
-  props: Object.keys(argTypes),
-  components: { unnnicButton },
-  template: '<unnnic-button v-bind="$props" />',
-});
-
-export const Primary = Template.bind({});
-Primary.args = {
-  text: 'Button Text',
-  disabled: false,
+export const Primary = {
+  args: {
+    text: 'Button Text',
+    disabled: false,
+  },
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  type: 'secondary',
-  text: 'Button Text',
-  disabled: false,
+export const Secondary = {
+  args: {
+    type: 'secondary',
+    text: 'Button Text',
+    disabled: false,
+  },
 };
 
-export const Tertiary = Template.bind({});
-Tertiary.args = {
-  type: 'tertiary',
-  text: 'Button Text',
-  disabled: false,
+export const Tertiary = {
+  args: {
+    type: 'tertiary',
+    text: 'Button Text',
+    disabled: false,
+  },
 };
 
-export const WithIcon = Template.bind({});
-WithIcon.args = {
-  text: 'Button Text',
-  iconLeft: 'developer-community-github-1-1',
+export const WithIcon = {
+  args: {
+    text: 'Button Text',
+    iconLeft: 'developer-community-github-1-1',
+  },
 };
 
-export const OnlyIcon = Template.bind({});
-OnlyIcon.args = {
-  iconCenter: 'add-1',
+export const OnlyIcon = {
+  args: {
+    iconCenter: 'add-1',
+  },
+};
+
+export const Alternative = {
+  args: {
+    type: 'alternative',
+    iconLeft: 'add',
+    text: 'Text',
+  },
+};
+
+export const Warning = {
+  args: {
+    type: 'warning',
+    iconLeft: 'add-1',
+    text: 'Text',
+  },
+};
+
+export const Attention = {
+  args: {
+    type: 'attention',
+    iconLeft: 'add-1',
+    text: 'Text',
+  },
 };
