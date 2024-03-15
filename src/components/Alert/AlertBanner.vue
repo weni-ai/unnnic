c1495f4fdde90328d31763373e8eb65c5b48058f
Marcus Vinícius <marcusvinici25@gmail.com>
2024-03-15 10:49:59 -0300

    changing code formatting unnic-icon

--- src/components/Alert/AlertBanner.vue
+++ src/components/Alert/AlertBanner.vue
@@ -4,13 +4,19 @@
         'banner-alert--banner-warning': type === 'warning',
       }]">
         <header v-show="text" class="banner-alert__container-text">
-          <unnnic-icon class="banner-alert__textIcon" v-show="isShowTextIcon(type)" :icon="getIconType(type)" size="sm" scheme="neutral-white" />
+          <unnnic-icon 
+            class="banner-alert__textIcon" 
+            v-show="isShowTextIcon(type)" 
+            :icon="getIconType(type)" 
+            size="sm" 
+            scheme="neutral-white" 
+          />
           <p class="text" >{{ text }}</p>
           <a v-if="linkHref" class="banner-alert__link" :href="linkHref" :target="linkTarget">
             {{ linkText }}
           </a>
         </header>
-  
+
         <div
           v-show="showCloseButton"
           class="banner-alert__close"
@@ -20,71 +26,71 @@
         </div>
       </section>
   </template>
-  
-  <script>
-  import UnnnicIcon from '../Icon.vue';
-  
-  export default {
-    components: {
-      UnnnicIcon,
-    },
-    methods: {
-      getIconType(type) {
-        if (type === 'danger') return 'block';
-        if (!type.trim()) return 'info'; //check if type is empty or whitespace
-        return type;
-      },
-      isShowTextIcon(type) {
-        return ['danger', 'warning', 'info', ''].includes(type)
-      },
-      emitClose() {
-        this.onClose();
 
-        this.$emit('close');
-      }
+<script>
+import UnnnicIcon from '../Icon.vue';
+
+export default {
+  components: {
+    UnnnicIcon,
+  },
+  methods: {
+    getIconType(type) {
+      if (type === 'danger') return 'block';
+      if (!type.trim()) return 'info'; // check if type is empty or whitespace
+      return type;
     },
-    props: {
-      text: {
-        type: String,
-        default: null,
-      }, 
-      onClose: {
-        type: Function,
-        default: () => {}
-      },
-      showCloseButton: {
-        type: Boolean,
-        default: false
-      },
-      linkHref: {
-        type: String,
-        default: '',
-      },
-      linkTarget: {
-        type: String,
-        default: '_blank',
-      },
-      linkText: {
-        type: String,
-        default: 'Learn more',
-      },
-      type: {
-        type: String,
-        default: 'info',
-      },
-    }
-  };
-  </script>
-  
+    isShowTextIcon(type) {
+      return ['danger', 'warning', 'info', ''].includes(type);
+    },
+    emitClose() {
+      this.onClose();
+
+      this.$emit('close');
+    },
+  },
+  props: {
+    text: {
+      type: String,
+      default: null,
+    },
+    onClose: {
+      type: Function,
+      default: () => {},
+    },
+    showCloseButton: {
+      type: Boolean,
+      default: false,
+    },
+    linkHref: {
+      type: String,
+      default: '',
+    },
+    linkTarget: {
+      type: String,
+      default: '_blank',
+    },
+    linkText: {
+      type: String,
+      default: 'Learn more',
+    },
+    type: {
+      type: String,
+      default: 'info',
+    },
+  },
+};
+</script>
+
   <style lang="scss" scoped>
   @import '../../assets/scss/unnnic.scss';
-  
+
   .banner-alert {
     display: grid;
     grid-template-columns: 1fr auto;
     text-align: center;
     margin-bottom: $unnnic-spacing-sm;
-  
+
     color: $unnnic-color-neutral-white;
     font-family: $unnnic-font-family-secondary;
     font-size: $unnnic-font-size-body-gt;
@@ -107,7 +113,7 @@
     &__textIcon {
       margin-right: $unnnic-spacing-xs;
     }
-  
+
     &__link {
       color: inherit;
       display: block;
@@ -115,7 +121,7 @@
       text-decoration: underline;
       text-underline-position: under;
     }
-  
+
     &__close {
       display: flex;
       align-items: center;
@@ -128,25 +134,25 @@
           background-color: $unnnic-color-aux-blue-300;
         }
     }
-  
+
     &--banner-warning {
       background-color: $unnnic-color-aux-orange-500;
-  
+
       .banner-alert__close {
         border-left: $unnnic-border-width-thinner solid $unnnic-color-aux-orange-300;
-  
+
         &:hover {
           background-color: $unnnic-color-aux-orange-300;
         }
       }
     }
-  
+
     &--banner-danger {
       background-color: $unnnic-color-aux-red-500;
-  
+
       .banner-alert__close {
         border-left: $unnnic-border-width-thinner solid $unnnic-color-aux-red-300;
-  
+
         &:hover {
           background-color: $unnnic-color-aux-red-300;
         }
@@ -154,4 +160,3 @@
     }
   }
   </style>
-  
