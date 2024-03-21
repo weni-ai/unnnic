da715343755b73305c83b3d4346ceffdd9afa9ba
Matheus Cristian <mathues@email.com>
2024-03-21 09:08:53 -0300

    feat(weni-eslint): adds weni eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,19 @@
 module.exports = {
   root: true,
-  env: {
-    node: true,
-  },
-  extends: ['plugin:vue/essential', '@vue/airbnb', 'plugin:storybook/recommended'],
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-  },
+  extends: ['@weni/eslint-config/vue2'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
-  overrides: [{
-    files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
-    env: {
-      jest: true,
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
     },
-  }],
+  ],
 };
