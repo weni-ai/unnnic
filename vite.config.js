44ad93371415bc1c3935bca031c4b54245785139
aldemylla <aldemylla@gmail.com>
2024-04-16 15:50:07 -0300

    fix: Attributes bugs removing vue/compat

--- vite.config.js
+++ vite.config.js
@@ -8,15 +8,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          compatConfig: {
-            MODE: 2,
-          },
-        },
-      },
-    }),
+    vue(),
     vueJsx(),
   ],
   build: {
@@ -39,7 +31,6 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      vue: '@vue/compat',
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
