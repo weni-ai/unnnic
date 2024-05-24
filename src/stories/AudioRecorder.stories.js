70063c0439f9aa1ba2abe3e273bbe11f957f3076
aldemylla <aldemylla@gmail.com>
2024-05-24 19:58:35 -0300

    fix: Adjusts AudioRecorder story

--- src/stories/AudioRecorder.stories.js
+++ src/stories/AudioRecorder.stories.js
@@ -5,44 +5,40 @@ export default {
   title: 'Form/AudioRecorder',
   component: unnnicAudioRecorder,
   argTypes: {},
-};
-
-const Template = (args, { argTypes }) => ({
-  props: Object.keys(argTypes),
-
-  components: {
-    unnnicAudioRecorder,
-  },
-
-  data() {
-    return {
-      audio: null,
-      status: '',
-    };
-  },
-
-  template: `
+  render: (args) => ({
+    components: {
+      unnnicAudioRecorder,
+    },
+    setup() {
+      return { args };
+    },
+    data() {
+      return {
+        audio: null,
+        status: '',
+      };
+    },
+    template: `
     <div>
       <button @click="$refs['audio-recorder'].record()">Gravar</button>
 
       <pre>v-model: {{ audio }}</pre>
       <pre>status: {{ status }}</pre>
 
-      <unnnic-audio-recorder v-bind="$props" ref="audio-recorder" v-model="audio" @status="(value) => this.status = value" />
+      <unnnic-audio-recorder v-bind="args" ref="audio-recorder" v-model="audio" @status="(value) => this.status = value" />
     </div>
   `,
-
-  methods: {},
-});
-
-export const Default = Template.bind({});
-
-Default.args = {
-  canDiscard: true,
+  }),
 };
 
-export const Player = Template.bind({});
+export const Default = {
+  args: {
+    canDiscard: true,
+  },
+};
 
-Player.args = {
-  src: AudioSample,
+export const Player = {
+  args: {
+    src: AudioSample,
+  },
 };
