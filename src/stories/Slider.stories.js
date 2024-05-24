780c37d4e26971c2d49e2ca9a69111bee1cd82ff
aldemylla <aldemylla@gmail.com>
2024-05-24 19:21:50 -0300

    feat: Update design unnnic Slider

--- src/stories/Slider.stories.js
+++ src/stories/Slider.stories.js
@@ -3,25 +3,21 @@ import unnnicSlider from '../components/Slider/Slider.vue';
 export default {
   title: 'Form/Slider',
   component: unnnicSlider,
+  argTypes: {
+    size: { control: { type: 'select', options: ['small', 'medium'] } },
+    textLeft: { control: { type: 'text' } },
+    textRight: { control: { type: 'text' } },
+    disabled: { control: { type: 'boolean' } },
+  },
 };
 
-const Template = (args, { argTypes }) => ({
-  props: Object.keys(argTypes),
-  components: { unnnicSlider },
-  template: '<unnnicSlider v-bind="$props" @valueChange="onChange"/>',
-  methods: {
-    onChange: (value) => {
-      console.log(value);
-    },
+export const Default = {
+  args: {
+    initialValue: 1,
+    minValue: 1,
+    maxValue: 4,
+    step: 1,
+    minLabel: '1',
+    maxLabel: '4',
   },
-});
-
-export const Default = Template.bind({});
-Default.args = {
-  initialValue: 1,
-  minValue: 1,
-  maxValue: 4,
-  step: 1,
-  minLabel: '1',
-  maxLabel: '4',
 };
