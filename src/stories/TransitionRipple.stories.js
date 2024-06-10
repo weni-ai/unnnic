9722c532c499ba16147a108eb27081baa17b41fe
aldemylla <aldemylla@gmail.com>
2024-06-10 18:08:30 -0300

    fix: Stories of transitions directory

--- src/stories/TransitionRipple.stories.js
+++ src/stories/TransitionRipple.stories.js
@@ -12,23 +12,24 @@ export default {
       },
     },
   },
-};
-
-const Template = (args, { argTypes }) => ({
-  props: Object.keys(argTypes),
-  components: {
-    unnnicTransitionRipple,
+  render: (args) => ({
+    setup() {
+      return { args };
+    },
+    components: {
+      unnnicTransitionRipple,
+    },
+    template: `
+      <div ref="transitionContainer" @mousedown="(event) => this.$refs.transitionRipple.addRipple(event)" style="width: 100%; height: 100px;">
+        Click to activate transition
+  
+        <unnnic-transition-ripple ref="transitionRipple" v-bind="args" />
+      </div>
+    `,
+  }),
+  args: {
+    color: 'weni-600',
   },
-  template: `
-    <div ref="transitionContainer" @mousedown="(event) => this.$refs.transitionRipple.addRipple(event)" style="width: 100%; height: 100px;">
-      Click to activate transition
-
-      <unnnic-transition-ripple ref="transitionRipple" v-bind="$props" />
-    </div>
-  `,
-});
-
-export const Default = Template.bind({});
-Default.args = {
-  color: 'weni-600',
 };
+
+export const Default = {};
