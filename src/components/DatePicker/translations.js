2136050b6d27d667e614b1ac36e01d082e9d9cab
aldemylla <aldemylla@gmail.com>
2024-06-11 12:13:10 -0300

    fix: Added translations to DatePicker

---
+++ src/components/DatePicker/translations.js
@@ -0,0 +1,131 @@
+export const months = {
+  'pt-br': [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro',
+  ],
+  en: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ],
+  es: [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre',
+  ],
+};
+
+export const days = {
+  'pt-br': ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+  en: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+  es: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
+};
+
+export const periods = {
+  'pt-br': [
+    {
+      name: 'Últimos 7 dias',
+      id: 'last-7-days',
+    },
+    {
+      name: 'Últimos 14 dias',
+      id: 'last-14-days',
+    },
+    {
+      name: 'Últimos 30 dias',
+      id: 'last-30-days',
+    },
+    {
+      name: 'Últimos 12 meses',
+      id: 'last-12-months',
+    },
+    {
+      name: 'Mês Atual',
+      id: 'current-month',
+    },
+    {
+      name: 'Personalizar',
+      id: 'custom',
+    },
+  ],
+  en: [
+    {
+      name: 'Last 7 days',
+      id: 'last-7-days',
+    },
+    {
+      name: 'Last 14 days',
+      id: 'last-14-days',
+    },
+    {
+      name: 'Last 30 days',
+      id: 'last-30-days',
+    },
+    {
+      name: 'Last 12 months',
+      id: 'last-12-months',
+    },
+    {
+      name: 'Current month',
+      id: 'current-month',
+    },
+    {
+      name: 'Custom',
+      id: 'custom',
+    },
+  ],
+  es: [
+    {
+      name: 'Últimos 7 días',
+      id: 'last-7-days',
+    },
+    {
+      name: 'Últimos 14 días',
+      id: 'last-14-days',
+    },
+    {
+      name: 'Últimos 30 días',
+      id: 'last-30-days',
+    },
+    {
+      name: 'Últimos 12 meses',
+      id: 'last-12-months',
+    },
+    {
+      name: 'Mes actual',
+      id: 'current-month',
+    },
+    {
+      name: 'Personalizar',
+      id: 'custom',
+    },
+  ],
+};
--- src/stories/DatePicker.stories.js
+++ src/stories/DatePicker.stories.js
@@ -27,50 +27,7 @@ export default {
 export const Default = {
   args: {
     size: 'large',
-    clearLabel: 'Limpar',
-    actionLabel: 'Filtrar',
-    months: [
-      'Janeiro',
-      'Fevereiro',
-      'Março',
-      'Abril',
-      'Maio',
-      'Junho',
-      'Julho',
-      'Agosto',
-      'Setembro',
-      'Outubro',
-      'Novembro',
-      'Dezembro',
-    ],
-    days: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
     initialStartDate: '12-01-2021',
     initialEndDate: '12-01-2021',
-    options: [
-      {
-        name: 'Últimos 7 dias',
-        id: 'last-7-days',
-      },
-      {
-        name: 'Últimos 14 dias',
-        id: 'last-14-days',
-      },
-      {
-        name: 'Últimos 30 dias',
-        id: 'last-30-days',
-      },
-      {
-        name: 'Últimos 12 meses',
-        id: 'last-12-months',
-      },
-      {
-        name: 'Mês Atual',
-        id: 'current-month',
-      },
-      {
-        name: 'Personalizar',
-        id: 'custom',
-      },
-    ],
   },
 };
