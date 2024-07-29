ae81c61ddcdf7155ecc5550412b027c1c55c38bd
Aldemylla <aldemylla@gmail.com>
2024-07-29 12:15:09 -0300

    feat: Checkbox docs

--- src/stories/Checkbox.stories.js
+++ src/stories/Checkbox.stories.js
@@ -1,35 +1,71 @@
-import unnnicCheckbox from '../components/Checkbox/Checkbox.vue';
+import { action } from '@storybook/addon-actions';
+import UnnnicCheckbox from '../components/Checkbox/Checkbox.vue';
 
 export default {
   title: 'Form/Checkbox',
-  component: unnnicCheckbox,
+  component: UnnnicCheckbox,
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: `Allows the user to select multiple options from a category. It can also be 
+        used to display a single option that may require additional acceptance or confirmation 
+        before submission.
+          `,
+      },
+    },
+  },
+  args: { disabled: false, size: 'md' },
   argTypes: {
-    value: { control: 'inline-radio', options: [true, false, 'less'] },
+    modelValue: { control: 'inline-radio', options: [true, false, 'less'] },
     disabled: { control: 'boolean' },
     size: { control: 'select', options: ['md', 'sm'] },
   },
   render: (args) => ({
     components: {
-      unnnicCheckbox,
+      UnnnicCheckbox,
     },
     setup() {
-      return { args };
-    },
-    data() {
-      return {
-        value: true,
+      const updateModelValue = (value) => {
+        action('update:modelValue')(value);
+        args.modelValue = value;
       };
+      return { args, updateModelValue };
     },
     template: `
-      <unnnicCheckbox v-model="value" />
+      <UnnnicCheckbox v-bind="args" @update:modelValue="updateModelValue"/>
     `,
   }),
 };
 
 export const Default = {
   args: {
-    value: true,
-    disabled: false,
-    size: 'md',
+    modelValue: false,
+  },
+};
+
+export const Selected = {
+  args: {
+    modelValue: true,
+  },
+};
+
+export const LessSelected = {
+  args: {
+    modelValue: 'less',
+  },
+};
+
+export const Disabled = {
+  args: {
+    modelValue: false,
+    disabled: true,
+  },
+};
+
+export const DisabledSelected = {
+  args: {
+    modelValue: true,
+    disabled: true,
   },
 };
