6a78c0240c8e58da186586e61500818a4ac6496b
Eduardo Medeiros <eduardomedeirosrn2@gmail.com>
2024-07-29 15:28:02 -0300

    fix: checkbox stories not working

--- src/stories/Checkbox.stories.js
+++ src/stories/Checkbox.stories.js
@@ -1,34 +1,30 @@
-import unnnicCheckbox from '../components/Checkbox/Checkbox.vue';
+import UnnnicCheckbox from '../components/Checkbox/Checkbox.vue';
 
 export default {
   title: 'Form/Checkbox',
-  component: unnnicCheckbox,
+  component: UnnnicCheckbox,
   argTypes: {
-    value: { control: 'inline-radio', options: [true, false, 'less'] },
+    modelValue: { control: 'inline-radio', options: [true, false, 'less'] },
     disabled: { control: 'boolean' },
     size: { control: 'select', options: ['md', 'sm'] },
+    textRight: { control: 'text' },
   },
   render: (args) => ({
     components: {
-      unnnicCheckbox,
+      UnnnicCheckbox,
     },
     setup() {
       return { args };
     },
-    data() {
-      return {
-        value: true,
-      };
-    },
     template: `
-      <unnnicCheckbox v-model="value" />
+      <UnnnicCheckbox v-bind="args" @change="args.modelValue = $event" />
     `,
   }),
 };
 
 export const Default = {
   args: {
-    value: true,
+    modelValue: true,
     disabled: false,
     size: 'md',
   },
