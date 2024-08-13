ef475fa69acd4c65c581b1d32294309c0efd9463
Eduardo Medeiros <eduardomedeirosrn2@gmail.com>
2024-08-13 15:25:12 -0300

    feat: add return emoji name storie

--- src/stories/EmojiPicker.stories.js
+++ src/stories/EmojiPicker.stories.js
@@ -1,5 +1,5 @@
-import unnnicButton from '../components/Button/Button.vue';
-import unnnicEmojiPicker from '../components/EmojiPicker/EmojiPicker.vue';
+import UnnnicButton from '../components/Button/Button.vue';
+import UnnnicEmojiPicker from '../components/EmojiPicker/EmojiPicker.vue';
 
 export default {
   title: 'Form/EmojiPicker',
@@ -12,11 +12,11 @@ export default {
 };
 
 export const Default = () => ({
-  components: { unnnicEmojiPicker, unnnicButton },
+  components: { UnnnicEmojiPicker, UnnnicButton },
   template: `
     <section style="position: relative;">
-      <unnnicButton @click.stop="handleEmojiPicker" type="secondary" iconCenter="emoji" size="small"/>
-      <unnnicEmojiPicker
+      <UnnnicButton @click.stop="handleEmojiPicker" type="secondary" iconCenter="emoji" size="small"/>
+      <UnnnicEmojiPicker
         v-show="isEmojiPickerOpen"
         @emojiSelected="handleInput"
         @close="closeEmojiPicker"
@@ -51,6 +51,54 @@ export const Default = () => ({
     handleInput(event) {
       if (typeof event === 'string') {
         this.inputValue += event;
+      } else {
+        this.inputValue = event.target.value;
+      }
+    },
+  },
+});
+
+export const EmojiName = () => ({
+  components: { UnnnicEmojiPicker, UnnnicButton },
+  template: `
+    <section style="position: relative;">
+      <UnnnicButton @click.stop="handleEmojiPicker" type="secondary" iconCenter="emoji" size="small"/>
+      <UnnnicEmojiPicker
+        v-show="isEmojiPickerOpen"
+        returnName
+        @emojiSelected="handleInput"
+        @close="closeEmojiPicker"
+      />
+      <input
+        placeholder="..."
+        :value="inputValue"
+        @input="handleInput"
+      />
+    </section>
+  `,
+  data() {
+    return {
+      isEmojiPickerOpen: false,
+      inputValue: '',
+    };
+  },
+  methods: {
+    openEmojiPicker() {
+      this.isEmojiPickerOpen = true;
+    },
+    closeEmojiPicker() {
+      this.isEmojiPickerOpen = false;
+    },
+    handleEmojiPicker() {
+      if (this.isEmojiPickerOpen) {
+        this.closeEmojiPicker();
+      } else {
+        this.openEmojiPicker();
+      }
+    },
+    handleInput(event) {
+      if (typeof event === 'string') {
+        this.inputValue = event;
       } else {
         this.inputValue = event.target.value;
       }
