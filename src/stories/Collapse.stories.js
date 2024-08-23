9d816ed34b8d3e79ffa3887ae530797e59d75c91
Aldemylla <aldemylla@gmail.com>
2024-08-23 10:45:14 -0300

    feat: Collapse docs

--- src/stories/Collapse.stories.js
+++ src/stories/Collapse.stories.js
@@ -1,8 +1,19 @@
 import UnnnicCollapse from '../components/Collapse/Collapse.vue';
 
 export default {
-  title: 'accordion/Collapse',
+  title: 'Data Display/Collapse',
   component: UnnnicCollapse,
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: `Allows for dynamic display and hiding of content. It enables sections of 
+        information to be hidden or revealed with a simple click or tap, optimizing space usage 
+        on the interface and enhancing the user experience.
+          `,
+      },
+    },
+  },
   argTypes: {
     'on-update:model-value': { action: '@update:model-value' },
     size: {
@@ -27,59 +38,13 @@ export default {
       };
     },
     template: `
-      <div>
-      <button @click="active = !active">External change active</button>
-
-      <pre>v-model: {{ active }}</pre>
-
-      <unnnic-collapse v-bind="args" v-model="active">
+      <UnnnicCollapse v-bind="args" v-model="active">
         Default slot
         <br />
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
         labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
         laboris nisi ut aliquip ex ea commodo consequat.
-      </unnnic-collapse>
-
-      <pre>
-
-      <b>Basic usage:</b>
-
-      &lt;unnnic-collapse title="Title"&gt;
-        Collapse body
-      &lt;/unnnic-collapse&gt;
-
-
-      <b  >With header slot:</b>
-
-      &lt;unnnic-collapse&gt;
-        &lt;template slot="header"&gt;Title&lt;/template&gt;
-
-        Collapse body
-      &lt;/unnnic-collapse&gt;
-
-
-      <b>With v-model:</b>
-
-      &lt;unnnic-collapse title="Title" v-model="active"&gt;
-        Collapse body
-      &lt;/unnnic-collapse&gt;
-
-
-      <b>Complete usage:</b>
-
-      &lt;unnnic-collapse
-        title           String
-        border-bottom   Boolean
-        unspaced-icon   Boolean
-        active          Boolean
-        @change         Boolean Event
-      &gt;
-        &lt;template slot="header"&gt;Title&lt;/template&gt;
-
-        Collapse body
-      &lt;/unnnic-collapse&gt;
-      </pre>  
-    </div>
+      </UnnnicCollapse>
     `,
   }),
 };
@@ -88,4 +53,29 @@ export const Default = {
   args: {
     title: 'Title prop or header slot',
   },
+};
+
+export const CustomHeader = {
+  render: (args) => ({
+    setup() {
+      return { args };
+    },
+    components: {
+      UnnnicCollapse,
+    },
+    data() {
+      return {
+        active: true,
+      };
+    },
+    template: `
+      <UnnnicCollapse v-bind="args" v-model="active">
+        <template #header>
+          <p :style="{ backgroundColor: '#C6FFF7', padding: '5px' }">Title Custom</p>
+        </template>
+
+        Collapse body
+      </UnnnicCollapse>
+    `,
+  }),
 };
