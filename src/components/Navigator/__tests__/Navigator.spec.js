1ff8f2ab801477bfb16034f84e26e9c8bced1063
Eduardo Medeiros <eduardomedeirosrn2@gmail.com>
2024-10-17 11:51:20 -0300

    fix: remove vi import

--- src/components/Navigator/__tests__/Navigator.spec.js
+++ src/components/Navigator/__tests__/Navigator.spec.js
@@ -1,5 +1,5 @@
 import { mount } from '@vue/test-utils';
-import { describe, it, expect, beforeEach, afterEach } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 
 import UnnnicNavigator from '../index.vue';
 
