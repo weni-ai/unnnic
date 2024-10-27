76bb646bb857e2f114c53e7db56d5f963c448f7d
MarcusViniciusLSantos <marcusvinici25@gmail.com>
2024-10-27 17:48:39 -0300

    adjusting pastes and imports

--- src/components/ChartFunnel/SvgFunnel/ChartFunnelThreeRows.vue
+++ src/components/ChartFunnel/SvgFunnel/ChartFunnelThreeRows.vue
@@ -1,15 +1,15 @@
 <template>
   <ChartFunnelBaseRow
-    class="unnnic-chart-funnel-four-rows"
+    class="unnnic-chart-funnel-three-rows"
     :rows="rows"
-    viewBox="0 0 365 224"
+    viewBox="0 0 375 222"
   />
 </template>
 
 <script>
 import ChartFunnelBaseRow from './ChartFunnelBaseRow.vue';
 export default {
-  name: 'UnnnicChartFunnelFourRows',
+  name: 'UnnnicChartFunnelThreeRows',
 
   components: {
     ChartFunnelBaseRow,
@@ -28,41 +28,32 @@ export default {
       return [
         {
           pathD:
-            'M356.374 0H8.57255C2.47804 0 -1.37827 6.54249 1.5734 11.8745L26 56H338.75L363.36 11.8984C366.336 6.56589 362.481 0 356.374 0Z',
+            'M362.938 0H12.025C6.30976 0 2.43799 5.81972 4.64626 11.0911L31 74H343.75L370.307 11.1122C372.535 5.83818 368.663 0 362.938 0Z',
           titleX: '50%',
-          titleY: '23',
+          titleY: '32',
           descriptionX: '50%',
-          descriptionY: '43',
+          descriptionY: '52',
           title: data[0].title,
           description: data[0].description,
         },
         {
-          pathD: 'M339 56H26L58.4654 112H306.535L339 56Z',
+          pathD: 'M344 74H31L63.4654 148H311.535L344 74Z',
           titleX: '50%',
-          titleY: '78',
+          titleY: '106',
           descriptionX: '50%',
-          descriptionY: '98',
+          descriptionY: '126',
           title: data[1].title,
           description: data[1].description,
         },
         {
-          pathD: 'M306.5 112H58.5L90.9564 168H275.708L306.5 112Z',
+          pathD:
+            'M311.5 148H63.5L93.8569 217.213C95.1325 220.122 98.0075 222 101.183 222H275.372C278.603 222 281.517 220.056 282.758 217.073L311.5 148Z',
           titleX: '50%',
-          titleY: '136',
+          titleY: '180',
           descriptionX: '50%',
-          descriptionY: '156',
+          descriptionY: '200',
           title: data[2].title,
           description: data[2].description,
-        },
-        {
-          pathD:
-            'M276 168H91L120.697 219.969C122.121 222.462 124.772 224 127.643 224H239.357C242.228 224 244.879 222.462 246.303 219.969L276 168Z',
-          titleX: '50%',
-          titleY: '190',
-          descriptionX: '50%',
-          descriptionY: '210',
-          title: data[3].title,
-          description: data[3].description,
         },
       ];
     },
@@ -71,9 +62,9 @@ export default {
 </script>
 
 <style lang="scss">
-@import '../../assets/scss/unnnic.scss';
+@import '../../../assets/scss/unnnic.scss';
 
-.unnnic-chart-funnel-four-rows {
+.unnnic-chart-funnel-three-rows {
   [class$='row']:nth-child(1) {
     fill: $unnnic-color-weni-950;
   }
@@ -81,9 +72,6 @@ export default {
     fill: $unnnic-color-weni-800;
   }
   [class$='row']:nth-child(3) {
-    fill: $unnnic-color-weni-700;
-  }
-  [class$='row']:nth-child(4) {
     fill: $unnnic-color-weni-600;
   }
 }
