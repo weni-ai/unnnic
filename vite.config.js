4dabc198f360a7abe26569a9ad56539290928420
Marcus Vinícius <marcusvinici25@gmail.com>
2024-11-28 15:00:17 -0300

    adjusting vite config

--- vite.config.js
+++ vite.config.js
@@ -1,58 +1,45 @@
 import { fileURLToPath, URL } from 'node:url';
-import { resolve } from 'path';
 
 import { defineConfig } from 'vite';
+import { federation } from '@module-federation/vite';
+import { dependencies } from './package.json';
+import path from 'path';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
-const testExcludes = [
-  '**/node_modules/**',
-  '**/*.stories.js',
-  '**/storybook-static/**',
-  '**/assets/**',
-  '**/.storybook/**',
-  '**/src/App.vue',
-  '**/src/index.js',
-  '**/src/main.js',
-  '**/src/utils/**',
-  '.eslintrc.js',
-];
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.js'),
-      name: 'Unnnic',
-      fileName: 'unnnic',
-    },
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/index.js'),
-      },
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    coverage: {
-      all: true,
-      provider: 'istanbul',
-      exclude: testExcludes,
-      reporter: ['text', 'json', 'html'],
-    },
-    exclude: testExcludes,
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      vue: path.resolve(
+        __dirname,
+        './node_modules/vue/dist/vue.runtime.esm-bundler.js',
+      ),
     },
   },
+  build: {
+    target: 'chrome89',
+  },
+  base: 'http://localhost:3003',
+  plugins: [
+    federation({
+      filename: 'remoteEntry.js',
+      name: 'remote',
+      exposes: {
+        './BaseStyles': './src/assets/scss/unnnic.scss',
+        './TextArea': './src/components/TextArea/TextArea.vue',
+      },
+      shared: {
+        ...dependencies,
+        vue: {
+          singleton: true,
+          requiredVersion: dependencies.vue,
+        },
+      },
+      remotes: {},
+    }),
+    vue(),
+    vueJsx(),
+  ],
 });
