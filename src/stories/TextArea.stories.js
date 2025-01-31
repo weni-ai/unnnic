e82f2923cf273a1be5990091f0667576f4725d4a
acnormun <anaclara.noronha@weni.ai>
2025-01-31 15:50:25 -0300

    fix: text area stories

--- src/stories/TextArea.stories.js
+++ src/stories/TextArea.stories.js
@@ -29,8 +29,8 @@ export default {
 
 export const Default = {
   args: {
-    label: 'Label <div> aaaa',
-    placeholder: 'Placeholder <div> aaaa',
+    label: 'Label',
+    placeholder: 'Placeholder',
     maxLength: 150,
     disabled: false,
     type: 'normal',
