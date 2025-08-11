5283ed8be96a82cdf5c941000378b9129c36ec27
MarcusViniciusLSantos <marcusvinici25@gmail.com>
2025-08-10 21:25:13 -0300

    chore: integrate TypeScript support in Vite configuration
    - Added `vite-plugin-dts` to generate TypeScript declaration files.
    - Updated entry points in the build configuration from JavaScript to TypeScript.
    - Adjusted plugin settings to include type definitions and exclude test files for better type management.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import dts from 'vite-plugin-dts';
 
 const testExcludes = [
   '**/node_modules/**',
@@ -20,16 +21,29 @@ const testExcludes = [
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    dts({
+      insertTypesEntry: true,
+      include: ['src/**/*'],
+      exclude: [
+        'src/**/*.stories.js',
+        'src/**/__tests__/**',
+        'src/**/*.spec.*',
+      ],
+      skipDiagnostics: true,
+    }),
+  ],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.js'),
+      entry: resolve(__dirname, 'src/index.ts'),
       name: 'Unnnic',
       fileName: 'unnnic',
     },
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.js'),
+        main: resolve(__dirname, 'src/index.ts'),
       },
       external: ['vue'],
       output: {
