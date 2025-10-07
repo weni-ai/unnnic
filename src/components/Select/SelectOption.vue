88460847c8969dd14b330558b056c3a0defbffa8
Mateus Eduardo Batista de Medeiros <eduardomedeirosrn2@gmail.com>
2025-10-07 10:52:23 -0300

    feat(Select): enhance keyboard navigation and option focus handling in UnnnicSelect component

--- src/components/Select/SelectOption.vue
+++ src/components/Select/SelectOption.vue
@@ -2,6 +2,7 @@
     <div :class="['unnnic-select-option', {
         'unnnic-select-option--disabled': props.disabled,
         'unnnic-select-option--active': props.active,
+        'unnnic-select-option--focused': props.focused,
     }]">
         <p class="unnnic-select-option__label">{{ props.label }}</p>
     </div>
@@ -16,11 +17,13 @@ interface SelectOptionProps {
     label: string;
     disabled?: boolean;
     active?: boolean;
+    focused?: boolean;
 }
 
 const props = withDefaults(defineProps<SelectOptionProps>(), {
     disabled: false,
     active: false,
+    focused: false,
 })
 </script>
 
@@ -37,6 +40,10 @@ const props = withDefaults(defineProps<SelectOptionProps>(), {
     border-radius: $unnnic-radius-1;
     padding: $unnnic-space-2 $unnnic-space-4;
     font: $unnnic-font-emphasis;
+
+    &--focused {
+        background-color: $unnnic-color-bg-soft;
+    }
 
     &--active {
         background-color: $unnnic-color-bg-active;
