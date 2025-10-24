73d31c90800810936536dad4f77546aea285541b
Aldemylla <aldemylla@gmail.com>
2025-10-23 21:57:04 -0300

    feat: enhance Popover component with new storybook examples and improve styling

--- src/components/ui/popover/PopoverContent.vue
+++ src/components/ui/popover/PopoverContent.vue
@@ -6,12 +6,11 @@
         cn(
           'unnnic-popover',
           `unnnic-popover--size-${props.size}`,
-          'z-50 bg-popover text-popover-foreground outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
-          props.class,
+          'bg-popover text-popover-foreground outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
         )
       "
     >
-      <section class="unnnic-popover__content">
+      <section :class="`unnnic-popover__content ${props.class || ''}`">
         <slot />
       </section>
 
@@ -62,6 +61,8 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits);
 $popover-space: $unnnic-space-4;
 
 .unnnic-popover {
+  z-index: 10000;
+
   border-radius: $unnnic-radius-2;
   box-shadow: $unnnic-shadow-1;
 
