5d3f70501524b5752280d516f6e8c26928c5f36e
Eduardo Medeiros <eduardomedeirosrn2@gmail.com>
2025-10-28 11:55:50 -0300

    [ENGAGE-5077] - Add template preview compontents (#682)
    * feat: add TemplatePreview and TemplatePreviewModal components with associated assets
    
    - Introduced TemplatePreview and TemplatePreviewModal components for enhanced template visualization.
    - Added new image assets for document, image, and video previews.
    - Updated tsconfig.json to include ES2021.String library.
    - Modified index.ts to register new components in the components map.
    
    * fix: update close icon scheme in ModalDialog snapshot
    
    - Changed the scheme of the close icon in the ModalDialog snapshot from 'neutral-darkest' to 'neutral-cloudy' for improved visual consistency.
    
    ---------
    
    Co-authored-by: Paulo Bernardo <paulo.bernardo@weni.ai>

---
+++ src/stories/TemplatePreview.stories.js
@@ -0,0 +1,94 @@
+import UnnnicTemplatePreview from "../components/TemplatePreview/TemplatePreview.vue";
+
+export default {
+  title: "example/TemplatePreview",
+  tags: ["autodocs"],
+  component: UnnnicTemplatePreview,
+};
+
+const Template = (args) => ({
+  components: { UnnnicTemplatePreview },
+  setup() {
+    return { args };
+  },
+  template: `
+      <unnnic-template-preview v-bind="args" />
+    `,
+});
+
+const bodyText =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sem orci, ultrices id lectus non, vehicula suscipit libero. Ut vehicula libero a tempus fringilla. Nam tincidunt vestibulum maximus. Morbi vel pretium risus. Curabitur id quam lectus. Quisque dolor elit, auctor a lacus non, rutrum ullamcorper mi. Nulla et turpis sed eros pharetra ullamcorper fermentum ac nisl. Donec lacinia hendrerit tellus, sit amet suscipit velit vestibulum sed. Praesent gravida posuere metus, ut fringilla dolor euismod vel. Aenean vel ipsum magna. Integer metus mi, fermentum ut nulla at, facilisis lobortis metus. Ut vitae tortor posuere, dapibus leo quis, finibus neque.";
+
+export const Default = Template.bind({});
+Default.args = {
+  template: {
+    header: {
+      type: "TEXT",
+      text: "Hello, world!",
+    },
+    footer: "Weni by VTEX",
+    buttons: [
+      {
+        text: "Button 1",
+        type: "PHONE_NUMBER",
+      },
+      {
+        text: "Button 2",
+        type: "URL",
+      },
+      {
+        text: "Button 3",
+        type: "QUICK_REPLY",
+      },
+    ],
+    body: bodyText,
+  },
+};
+
+export const Image = Template.bind({});
+Image.args = {
+  template: {
+    header: {
+      type: "MEDIA",
+      mediaType: "IMAGE",
+    },
+    footer: "Weni by VTEX",
+    body: bodyText,
+  },
+};
+
+export const Video = Template.bind({});
+Video.args = {
+  template: {
+    header: {
+      type: "MEDIA",
+      mediaType: "VIDEO",
+    },
+    buttons: [
+      {
+        text: "Button 1",
+        type: "PHONE_NUMBER",
+      },
+      {
+        text: "Button 2",
+        type: "URL",
+      },
+      {
+        text: "Button 3",
+        type: "QUICK_REPLY",
+      },
+    ],
+  },
+};
+
+export const Document = Template.bind({});
+Document.args = {
+  template: {
+    header: {
+      type: "MEDIA",
+      mediaType: "DOCUMENT",
+    },
+    footer: "Weni by VTEX",
+    body: bodyText,
+  },
+};
