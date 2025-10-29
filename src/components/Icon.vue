36525c441a2a014c9fabbfa3c586b92763d46729
Aldemylla <aldemylla@gmail.com>
2025-10-29 12:25:48 -0300

    feat: enhance Icon component to support custom SVG icons and improve styling

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,10 +1,28 @@
 <template>
-  <span
-    v-if="materialSymbolsName"
+  <component
+    :is="customSvgIcon"
+    v-if="customSvgIcon"
     :class="[
+      'unnnic-icon',
+      `unnnic-icon--size-svg-${size}`,
+      `unnnic-icon__size--${size}`,
+      clickable ? 'unnnic--clickable' : '',
+      scheme ? `unnnic-icon-scheme--${scheme}` : '',
+    ]"
+    data-testid="custom-icon"
+    @click="onClick"
+    @mousedown="(event) => $emit('mousedown', event)"
+    @mouseup="(event) => $emit('mouseup', event)"
+  />
+
+  <span
+    v-else-if="materialSymbolsName"
+    :class="[
+      'unnnic-icon',
       'material-symbols-rounded',
       `unnnic-icon-scheme--${scheme}`,
       `unnnic-icon-size--${size}`,
+      `unnnic-icon__size--${size}`,
       {
         'unnnic--clickable': clickable,
         'material-symbols-rounded--filled': filled,
@@ -22,7 +40,10 @@
 
 <script setup lang="ts">
 import { computed } from 'vue';
+
+import icons from '../utils/icons';
 import OldIconsMap from './Icon/OldIconsMap.json';
+
 import type { IconProps, IconSize } from './Icon/types';
 import type { SchemeColor } from '@/types/scheme-colors';
 
@@ -46,11 +67,12 @@ const emit = defineEmits<{
   mouseup: [event: Event];
 }>();
 
-const materialSymbolsName = computed(() => {
-  return (
-    (OldIconsMap as Record<string, string>)[props.icon as string] || props.icon
-  );
-});
+const customSvgIcon = computed(() => icons[props.icon as string] ?? null);
+
+const materialSymbolsName = computed(
+  () =>
+    (OldIconsMap as Record<string, string>)[props.icon as string] || props.icon,
+);
 
 const onClick = (event: Event) => {
   if (!props.clickable) return;
@@ -70,19 +92,20 @@ const onClick = (event: Event) => {
 
 <style lang="scss">
 @use '@/assets/scss/unnnic' as *;
+@use '@/assets/scss/icon-sizes' as *;
 
 $icon-sizes:
-  'xl' $unnnic-icon-size-xl,
-  'lg' $unnnic-icon-size-lg,
-  'md' $unnnic-icon-size-md,
-  'ant' $unnnic-icon-size-ant,
-  'sm' $unnnic-icon-size-sm,
-  'xs' $unnnic-icon-size-xs,
-  'avatar-lg' $unnnic-avatar-size-lg,
-  'avatar-md' $unnnic-avatar-size-md,
-  'avatar-sm' $unnnic-avatar-size-sm,
-  'avatar-xs' $unnnic-avatar-size-xs,
-  'avatar-nano' $unnnic-avatar-size-nano;
+  'xs' $unnnic-icon-size-3,
+  'sm' $unnnic-icon-size-4,
+  'ant' $unnnic-icon-size-5,
+  'md' $unnnic-icon-size-6,
+  'lg' $unnnic-icon-size-7,
+  'xl' $unnnic-icon-size-10,
+  'avatar-nano' $unnnic-icon-size-5,
+  'avatar-xs' $unnnic-icon-size-7,
+  'avatar-sm' $unnnic-icon-size-10,
+  'avatar-md' '72px',
+  'avatar-lg' '104px';
 
 @font-face {
   font-family: 'Material Symbols Rounded';
@@ -97,6 +120,33 @@ $icon-sizes:
   font-weight: 300;
   src: url('../assets/fonts/Material Symbols Rounded Filled.woff2')
     format('woff2');
+}
+
+.unnnic-icon {
+  @each $name, $color in $unnnic-scheme-colors {
+    &.unnnic-icon-scheme--#{$name} {
+      color: $color;
+
+      & .primary {
+        fill: $color;
+      }
+
+      & .primary-stroke {
+        stroke: $color;
+      }
+    }
+  }
+
+  @each $name, $size in $icon-sizes {
+    &.unnnic-icon-size--#{$name} {
+      font-size: $size;
+    }
+
+    &--size-svg-#{$name} {
+      width: $size;
+      height: $size;
+    }
+  }
 }
 
 .material-symbols-rounded {
@@ -118,18 +168,6 @@ $icon-sizes:
 
   &--filled {
     font-family: 'Material Symbols Rounded Filled';
-  }
-
-  @each $name, $color in $unnnic-scheme-colors {
-    &.unnnic-icon-scheme--#{$name} {
-      color: $color;
-    }
-  }
-
-  @each $name, $size in $icon-sizes {
-    &.unnnic-icon-size--#{$name} {
-      font-size: $size;
-    }
   }
 }
 </style>
