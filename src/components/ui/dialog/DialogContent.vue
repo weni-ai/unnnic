fa87484cbf28f4ce7a6ef84ceb92bdfd231b1dea
Aldemylla <aldemylla@gmail.com>
2025-11-11 10:56:27 -0300

    feat: Introduce parentClass prop in DialogContent for temporary styling adjustments

--- src/components/ui/dialog/DialogContent.vue
+++ src/components/ui/dialog/DialogContent.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { DialogContentEmits, DialogContentProps } from 'reka-ui';
-import type { HTMLAttributes } from 'vue';
+import type { HTMLAttributes, Component } from 'vue';
+import { computed, h } from 'vue';
 import { reactiveOmit } from '@vueuse/core';
 import {
   DialogContent,
@@ -13,19 +14,40 @@ import { cn } from '@/lib/utils';
 const props = withDefaults(
   defineProps<
     DialogContentProps & {
+      parentClass?: HTMLAttributes['class'];
       class?: HTMLAttributes['class'];
       size?: 'small' | 'medium' | 'large';
     }
   >(),
   {
     size: 'medium',
+    parentClass: '', // This class should be temporary, to be discontinued with the ModalDialog component
+    class: '',
   },
 );
 const emits = defineEmits<DialogContentEmits>();
 
-const delegatedProps = reactiveOmit(props, 'class');
+const delegatedProps = reactiveOmit(props, 'class', 'parentClass');
 
 const forwarded = useForwardPropsEmits(delegatedProps, emits);
+
+const contentClasses = computed(() =>
+  cn(
+    'unnnic-dialog-content',
+    `unnnic-dialog-content--size-${props.size}`,
+    'duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
+    props.class,
+  ),
+);
+
+// This component is used to wrap the DialogContent component in a section if the parentClass is provided
+// This is a temporary solution, to be discontinued with the ModalDialog component
+const ConditionalWrapper: Component = (_, { slots }) => {
+  const content = slots.default?.();
+  return props.parentClass
+    ? h('section', { class: props.parentClass }, content)
+    : content;
+};
 </script>
 
 <template>
@@ -33,20 +55,15 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits);
     <DialogOverlay
       class="unnnic-dialog-overlay data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
     />
-    <DialogContent
-      v-bind="forwarded"
-      :class="
-        cn(
-          'unnnic-dialog-content',
-          `unnnic-dialog-content--size-${props.size}`,
-          'duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
 
-          props.class,
-        )
-      "
-    >
-      <slot />
-    </DialogContent>
+    <ConditionalWrapper>
+      <DialogContent
+        v-bind="forwarded"
+        :class="contentClasses"
+      >
+        <slot />
+      </DialogContent>
+    </ConditionalWrapper>
   </DialogPortal>
 </template>
 
@@ -70,6 +87,8 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits);
   position: fixed;
   top: 50%;
   left: 50%;
+
+  overflow: hidden;
 
   transform: translate(-50%, -50%);
 
