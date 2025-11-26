a6ff6aa24cad96b24df578d962fc5c35bcf59714
Marcus Vinícius <marcusvinici25@gmail.com>
2025-11-26 19:34:34 -0300

    feat: add CustomRelativePeriodsWithBaseDate story to DatePicker with new periodBaseDate control for enhanced date selection options

--- src/stories/DatePicker.stories.js
+++ src/stories/DatePicker.stories.js
@@ -20,6 +20,7 @@ export default {
     months: { control: { type: 'array' } },
     days: { control: { type: 'array' } },
     options: { control: { type: 'array' } },
+    periodBaseDate: { control: { type: 'text' } },
     clearLabel: { control: { type: 'text' } },
     actionLabel: { control: { type: 'text' } },
   },
@@ -31,5 +32,25 @@ export const Default = {
     maxDate: moment().format('YYYY-MM-DD'),
     initialStartDate: '12-01-2021',
     initialEndDate: '12-01-2021',
+  },
+};
+
+export const CustomRelativePeriodsWithBaseDate = {
+  args: {
+    size: 'large',
+
+    periodBaseDate: moment().subtract(1, 'day').format('YYYY-MM-DD'),
+
+    maxDate: moment().subtract(1, 'day').format('YYYY-MM-DD'),
+
+    options: [
+      { name: 'Last 7 days (up to 24h ago)', id: 'last-7-days' },
+      { name: 'Last 14 days (up to 24h ago)', id: 'last-14-days' },
+      { name: 'Last 30 days (up to 24h ago)', id: 'last-30-days' },
+      { name: 'Last 12 months (up to 24h ago)', id: 'last-12-months' },
+      { name: 'Current month (up to 24h ago)', id: 'current-month' },
+      { name: 'Previous month (up to 24h ago)', id: 'previous-month' },
+      { name: 'Custom', id: 'custom' },
+    ],
   },
 };
