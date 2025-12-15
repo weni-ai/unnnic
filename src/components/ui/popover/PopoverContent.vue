7095ed9ffd4cff74251bf64e8ee4d704685130d1
Mateus Eduardo Batista de Medeiros <eduardomedeirosrn2@gmail.com>
2025-12-15 16:58:34 -0300

    feat: add customizable width prop to PopoverContent and update styles accordingly

--- src/components/ui/popover/PopoverContent.vue
+++ src/components/ui/popover/PopoverContent.vue
@@ -2,10 +2,10 @@
   <PopoverPortal>
     <PopoverContent
       v-bind="{ ...forwarded, ...$attrs }"
+      :style="{ width: contentWidth }"
       :class="
         cn(
           'unnnic-popover',
-          `unnnic-popover--size-${props.size}`,
           'bg-popover text-popover-foreground outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
         )
       "
@@ -43,7 +43,8 @@ const props = withDefaults(
   defineProps<
     PopoverContentProps & {
       class?: HTMLAttributes['class'];
-      size?: 'small' | 'medium' | 'large';
+      size?: string;
+      width?: string;
     }
   >(),
   {
@@ -51,6 +52,7 @@ const props = withDefaults(
     sideOffset: 4,
     size: 'medium',
     class: '',
+    width: '',
   },
 );
 const emits = defineEmits<PopoverContentEmits>();
@@ -79,6 +81,18 @@ const footerChildren = computed(() => {
     (vnode: VNode) => getComponentName(vnode) === 'UnnnicPopoverFooter',
   );
 });
+
+const contentWidth = computed(() => {
+  if (props.width) return props.width;
+
+  const sizes = {
+    small: '240px',
+    medium: '320px',
+    large: '400px',
+  };
+
+  return sizes[props.size as keyof typeof sizes];
+});
 </script>
 
 <style lang="scss">
@@ -105,16 +119,6 @@ $popover-space: $unnnic-space-4;
   &::-webkit-scrollbar-track {
     background: $unnnic-color-neutral-soft;
     border-radius: $unnnic-border-radius-pill;
-  }
-
-  &--size-small {
-    width: 240px;
-  }
-  &--size-medium {
-    width: 320px;
-  }
-  &--size-large {
-    width: 400px;
   }
 
   &__content {
