10dc47bd8ac0d8751ce297b0039047d1d2d20ffd
Aldemylla <aldemylla@gmail.com>
2026-02-19 17:05:06 -0300

    chore: Remove DocsTemplate.mdx and update Storybook preview configuration to eliminate unused documentation reference

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,12 @@
 import { setup } from '@storybook/vue3';
 
 import i18n from '../src/utils/plugins/i18n';
-import DocsTemplate from './DocsTemplate.mdx';
 import '../src/assets/scss/unnnic.scss';
 import '../src/assets/scss/tailwind.scss';
 
 export default {
   parameters: {
     docs: {
-      page: DocsTemplate,
       toc: true,
     },
   },
